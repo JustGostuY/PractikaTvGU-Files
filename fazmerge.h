861b7b9c7fc0020d60a1976ab7f639b6ea1b85f4
JustGostuY <57565094+JustGostuY@users.noreply.github.com>
2023-06-01 06:29:36 +0300

    Update fazmerge.h

--- fazmerge.h
+++ fazmerge.h
@@ -3,27 +3,30 @@ private:
     std::vector<std::vector<int>> tapes; // "ленты"
     std::vector<int> output; // результирующая последовательность
 public:
+    MultiwayMerge(std::vector<std::vector<int>> tapes) : tapes(tapes) {
+        for (auto& tape : tapes) {
+            MergeSort(tape);
+        }
+        merge(); // сортировка
+    }
+
     MultiwayMerge(std::vector<std::string> filepaths, std::string outputFilePath) {
         // чтение данных из файлов и сохранение их в ленты
         for (const auto& filepath : filepaths) {
             std::ifstream file(filepath, std::ios::binary);
             std::vector<int> tape;
 
-            while (true) {
-                int num;
-                file.read((char*)&num, sizeof(num));
-
-                if (file.eof()) {
-                    break;
-                }
-
+            int num;
+            while (file >> num) {
                 tape.push_back(num);
             }
 
             file.close();
-            std::sort(tape.begin(), tape.end());
-
+            
+            //Сортруем ленту
+            MergeSort(tape);
             tapes.push_back(tape);
+            tape.clear();
         }
 
         merge(); // сортировка
@@ -32,29 +35,72 @@ public:
 
     ~MultiwayMerge() {}
 
-    void merge() {
-        std::priority_queue<
-            std::pair<int, int>,
-            std::vector<std::pair<int, int>>,
-            std::greater<std::pair<int, int>>
-        > heap; // куча сортировки
+    // Сортировка слиянием ////
+    void MergeSortImpl(std::vector<int>& values, std::vector<int>& buffer, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            MergeSortImpl(values, buffer, l, m);
+            MergeSortImpl(values, buffer, m + 1, r);
 
-        for (int i = 0; i < tapes.size(); i++) { // заполнение кучи первыми элементами из каждой ленты
+            int k = l;
+            for (int i = l, j = m + 1; i <= m || j <= r; ) {
+                if (j > r || (i <= m && values[i] < values[j])) {
+                    buffer[k] = values[i];
+                    ++i;
+                }
+                else {
+                    buffer[k] = values[j];
+                    ++j;
+                }
+                ++k;
+            }
+            for (int i = l; i <= r; ++i) {
+                values[i] = buffer[i];
+            }
+        }
+    }
+
+    void MergeSort(std::vector<int>& values) {
+        if (!values.empty()) {
+            std::vector<int> buffer(values.size());
+            MergeSortImpl(values, buffer, 0, values.size() - 1);
+        }
+    }
+    //////////////////////////
+    void merge() {
+        // Количество лент и все ленты, которые нужно объединить
+        int k = tapes.size();
+        std::vector<int> indices(k, 0);
+        std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
+        std::queue<int> q;
+
+        // Наполняем очередь первыми элементами каждой ленты
+        for (int i = 0; i < k; i++) {
             if (!tapes[i].empty()) {
-                heap.push(std::make_pair(tapes[i][0], i));
+                min_heap.push(tapes[i][0]);
+                q.push(i);
             }
         }
 
-        while (!heap.empty()) {
-            int element = heap.top().first;
-            int tapeIndex = heap.top().second;
-            heap.pop();
+        // Начинаем слияние
+        while (!min_heap.empty()) {
+            // Извлекаем минимальный элемент из кучи
+            int min_elem = min_heap.top();
+            min_heap.pop();
 
-            output.push_back(element);
-            tapes[tapeIndex].erase(tapes[tapeIndex].begin()); // удаляем из ленты элемент, который уже был добавлен в рез-тат
+            // Добавляем этот элемент в выходную последовательность
+            output.push_back(min_elem);
+            // Сортируем слиянием после добавление элемента;
+            MergeSort(output);
+            // Получаем номер ленты и увеличиваем соответствующий индекс
+            int tape = q.front();
+            indices[tape]++;
+            q.pop();
 
-            if (!tapes[tapeIndex].empty()) { // добавляем следующий элемент из ленты, если там что-то осталось
-                heap.push(std::make_pair(tapes[tapeIndex][0], tapeIndex));
+            // Добавляем следующий элемент этой ленты в кучу
+            if (indices[tape] < tapes[tape].size()) {
+                min_heap.push(tapes[tape][indices[tape]]);
+                q.push(tape);
             }
         }
     }
